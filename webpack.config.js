fd766d4d165bde551a0eb57bbf75bc52a0f9a742
yanxiaodi <929213769@qq.com>
2016-09-05 23:42:24 +0800

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname); //当前文件路径
+var REACT_PATH = path.resolve(ROOT_PATH,'react'); //项目文件夹
+var REACT_BUILD_PATH = path.resolve(ROOT_PATH,'react_build'); //打包后的文件夹
+
 module.exports = {
     entry:[
         'webpack/hot/dev-server',
